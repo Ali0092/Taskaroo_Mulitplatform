9a4028ba8ed8f7bafa615092d1e31782271eb8ce
Ali0092 <aligondal8750@gmail.com>
2025-12-30 17:25:37 +0500

    completed v1 taskaroo

--- composeApp/src/commonMain/kotlin/com/dev/taskaroo/App.kt
+++ composeApp/src/commonMain/kotlin/com/dev/taskaroo/App.kt
@@ -11,28 +11,12 @@
  */
 package com.dev.taskaroo
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.dev.taskaroo.database.ProvideDatabaseHelper
-import com.dev.taskaroo.screens.IntroScreen
 import com.dev.taskaroo.screens.MainScreen
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import taskaroo.composeapp.generated.resources.Res
-import taskaroo.composeapp.generated.resources.compose_multiplatform
 
 /**
  * The root composable function of the Taskaroo application.
